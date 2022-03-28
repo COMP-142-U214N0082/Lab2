ab318d2ffe558a3a18f8251a8694aa2b3d24ec86
AndOffline <pogodaev.and@gmail.com>
2022-03-28 18:43:23 +0300

    Added the doc folder with DoxyGen content

---
+++ doc/html/search/functions_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['drawhorizontalline_0',['drawHorizontalLine',['../_lab2_8cpp.html#ae25a0bb1fce4ac5203fcde37f54d6550',1,'Lab2.cpp']]],
+  ['drawrectangle_1',['drawRectangle',['../_lab2_8cpp.html#a807abad39967690fede3831e3aa61cab',1,'Lab2.cpp']]],
+  ['drawsquare_2',['drawSquare',['../_lab2_8cpp.html#ae85226dc074fb12e47d8eb29028297cb',1,'Lab2.cpp']]],
+  ['drawverticalline_3',['drawVerticalLine',['../_lab2_8cpp.html#a3374a71928ea4274b3a30c8c6fcbed5a',1,'Lab2.cpp']]]
+];
